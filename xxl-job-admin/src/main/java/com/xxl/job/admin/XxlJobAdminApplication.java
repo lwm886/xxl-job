5af6f1a942c77588556caa1146ca63007921bf33
lwm <227959626@qq.com>
2020-10-28 17:51:17 +0800

    基于apollo改造

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -1,11 +1,13 @@
 package com.xxl.job.admin;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
+@EnableApolloConfig
 @SpringBootApplication
 public class XxlJobAdminApplication {
 
